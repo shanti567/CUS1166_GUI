8889f2be156c30a733a9aed00a95c1a099febbd7
Giannaskye <gianna.sheppard22@my.stjohns.edu>
2026-02-20 16:20:37 -0500

    Add files via upload

---
+++ VehicleCloudFrame.java
@@ -0,0 +1,46 @@
+import java.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+public class VehicleCloudFrame extends JFrame{
+
+
+private static JFrame frame;
+private static JButton submitButton;
+private static JButton clearButton;
+private static JRadioButton ownerButton;
+private static JRadioButton clientButton;
+private static JTextField idField;
+
+public VehicleCloudFrame (){
+    
+    setupFrame();        // TO:DO - shanti
+    createComponents();  // TO:DO - hawa
+    attachListeners();   // TO:DO - gianna
+
+    frame.setVisible(true);
+}
+
+//TO:DO - SHANTI - frame + design 
+private static void setupFrame(){
+    //start code here
+
+}
+//TO:DO - HAWA - panels, buttons, textfields 
+private static void createComponents(){
+    // start code here 
+}
+
+
+
+
+//TO:DO- GIANNA - action listenrs
+private static void attachListeners(){
+    //start code here 
+
+}
+}
